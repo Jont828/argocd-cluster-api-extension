4c3fce36bfe04c3586e648698679ab9238c13bff
Jont828 <jt572@cornell.edu>
2024-03-19 16:39:50 -0400

    Add axios and attempt to get list of apps

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,12 +1,29 @@
 import * as React from "react";
 import Tree from "./components/Tree.jsx";
+import { default as axios } from 'axios';
 
 export const Extension = (props) => {
+  const [apps, setApps] = React.useState(null);
+
+  React.useEffect(async () => {
+    let result = await getApplications();
+    setApps(result.items);
+  }, []);
+
+  if (!apps) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div id="root">
-      <h1>Hello world!</h1>
-      <p>Find me in ./web/src/index.js</p>
-      <Tree />
+      <h1>Applications:</h1>
+      <ul>
+        {apps.map(app => (
+          <li key={app.metadata.name}>
+            <h2>{app.metadata.name}</h2>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
@@ -21,3 +38,41 @@ export const component = Extension;
     "fa-cloud"
   );
 })(window);
+
+const getResource = (name, appNamespace, resource) => {
+  const params = {
+    name,
+    appNamespace,
+    namespace: resource.namespace,
+    resourceName: resource.name,
+    version: resource.version,
+    kind: resource.kind,
+    group: resource.group || ''
+  };
+
+  return axios.get(`/api/v1/applications/${name}/resource`, { params }).then(response => {
+    const { manifest } = response.data;
+    return JSON.parse(manifest);
+  });
+};
+
+const getApplications = () => {
+  // const params = {
+  //   name: name,
+  //   refresh: refresh,
+  //   projects: projects,
+  //   resourceVersion: resourceVersion,
+  //   selector: selector,
+  //   repo: repo,
+  //   appNamespace: appNamespace,
+  //   project: project,
+  // };
+
+  return axios.get(`/api/v1/applications`).then(response => {
+    const { manifest } = response.data;
+    return JSON.parse(manifest);
+  }).catch(err => {
+    console.log(err);
+    return err;
+  });
+};
